bea7ecf3a2372c2d752828933f689cc9034dc10e
Adrian Palacios <78890986+AdrianP17@users.noreply.github.com>
2025-09-22 00:49:19 -0500

    chore: add Scalar.AspNetCore package and configure API reference

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -10,6 +11,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
