f3a5b2852bfb70b1d6ecd774068cce0bd4a13b15
sofia <ssarmiento088@gmail.com>
2025-10-04 22:00:18 -0500

    feat: add reseñas mappers

---
+++ Mappers/ResenaMappers.cs
@@ -0,0 +1,31 @@
+using reseñas.Dtos.Resenas;
+using reseñas.Models;
+
+namespace reseñas.Mappers
+{
+    public static class ResenaMappers
+    {
+        public static ResenaDto ToResenaDto(this Resena resenaModel)
+        {
+            return new ResenaDto
+            {
+                Id = resenaModel.Id,
+                Id_detalle_orden = resenaModel.Id_detalle_orden,
+                Comentario = resenaModel.Comentario,
+                Calificacion = resenaModel.Calificacion,
+                Fecha_resena = resenaModel.Fecha_resena.ToUniversalTime()
+            };
+        }
+
+        public static Resena ToPromocionFromCreateDto(this CreateResenaRequestDto resenaDto)
+        {
+            return new Resena
+            {
+                Id_detalle_orden = resenaDto.Id_detalle_orden,
+                Comentario = resenaDto.Comentario,
+                Calificacion = resenaDto.Calificacion,
+                Fecha_resena = resenaDto.Fecha_resena.ToUniversalTime()
+            };
+        }
+    }
+}
